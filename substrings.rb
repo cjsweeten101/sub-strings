2aa6a1534575cc319e7e6ce5200e56865db896a0
Clayton Sweeten <cjsweeten101@gmail.com>
2024-10-16 11:27:55 -0600

    Substring counts multiple matches

--- substrings.rb
+++ substrings.rb
@@ -1,5 +1,11 @@
 def substrings(search_string, substring_dict)
-  substring_dict.select do |word|
-    search_string.include?(word)
+  result_hash = {}
+  search_string.downcase.split.each do |word|
+    matches = substring_dict.each do |dict_word|
+      if word.include?(dict_word) 
+        result_hash[dict_word] ? result_hash[dict_word] += 1 : result_hash[dict_word] = 1
+      end
+    end
   end
+  result_hash
 end
